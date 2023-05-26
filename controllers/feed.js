c4a463d53011261a804ba0a047522eb3ad50ec47
sahanenes <sahanenesss@gmail.com>
2023-05-26 19:58:14 +0300

    validation

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,15 +1,40 @@
+const { validationResult } = require("express-validator");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post!' }]
+    posts: [
+      {
+        _id: "1",
+        title: "First Post",
+        content: "This is the first post!",
+        imageUrl: "images/eggs.jpg",
+        creator: {
+          name: "Enes",
+        },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    return res
+      .status(422)
+      .json({ message: "validation error", error: error.array() });
+  }
   const title = req.body.title;
   const content = req.body.content;
   // Create post in db
   res.status(201).json({
-    message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content }
+    message: "Post created successfully!",
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: { name: "Enes" },
+      createdAt: new Date(),
+    },
   });
 };
--- routes/feed.js
+++ routes/feed.js
@@ -1,13 +1,21 @@
-const express = require('express');
+const express = require("express");
+const { query } = require("express-validator");
 
-const feedController = require('../controllers/feed');
+const feedController = require("../controllers/feed");
 
 const router = express.Router();
 
 // GET /feed/posts
-router.get('/posts', feedController.getPosts);
+router.get("/posts", feedController.getPosts);
 
 // POST /feed/post
-router.post('/post', feedController.createPost);
+router.post(
+  "/post",
+  [
+    query("title").trim().isLength({ min: 5 }),
+    query("content").trim().isLength({ min: 5 }),
+  ],
+  feedController.createPost
+);
 
 module.exports = router;
